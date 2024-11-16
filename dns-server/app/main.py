e3a03b2c75c8cd389fe190707422edb17c458038
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-11-16 10:25:08 +0545

    feat: send example dns message

--- dns-server/app/main.py
+++ dns-server/app/main.py
@@ -1,5 +1,7 @@
 import socket
 
+from app import message
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,9 +13,28 @@ def main():
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
+            msg = message.DnsMessage.from_bytes(buf)
+            example_response = message.DnsMessage(
+                header=message.Header(
+                    id=msg.header.id,
+                    flags=message.Flags(
+                        qr=message.QR_REPLY_PACKET,
+                        opcode=0,
+                        aa=0,
+                        tc=0,
+                        rd=0,
+                        ra=0,
+                        z=0,
+                        rcode=0,
+                    ),
+                    qdcount=0,
+                    ancount=0,
+                    nscount=0,
+                    arcount=0,
+                )
+            )
 
-            response = b""
-
+            response = example_response.encode()
             udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")
