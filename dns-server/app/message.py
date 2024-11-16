9f81146a9d29d9730be2aac76c2bf16970bbd157
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-11-16 10:25:25 +0545

    feat: add encoder for header, flags and dns message

---
+++ dns-server/app/message.py
@@ -0,0 +1,109 @@
+from dataclasses import dataclass
+from typing import Literal, cast
+
+from app import utils
+
+import io
+
+
+FOUR_BIT_INT = Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+THREE_BIT_INT = Literal[0, 1, 2, 3, 4, 5, 6, 7]
+ONE_BIT_INT = Literal[0, 1]
+
+
+QR_REPLY_PACKET = 1
+QR_QUESTION_PACKET = 0
+
+
+@dataclass()
+class Flags:
+    qr: ONE_BIT_INT
+    opcode: FOUR_BIT_INT
+    aa: ONE_BIT_INT
+    tc: ONE_BIT_INT
+    rd: ONE_BIT_INT
+    ra: ONE_BIT_INT
+    z: THREE_BIT_INT
+    rcode: FOUR_BIT_INT
+
+    @classmethod
+    def from_bytes(cls, b_flag: bytes) -> "Flags":
+        bits = utils.bytes_to_bits(b_flag)
+        return cls(
+            qr=cast(ONE_BIT_INT, int(bits[0])),
+            opcode=cast(FOUR_BIT_INT, int(bits[1:5], 2)),
+            aa=cast(ONE_BIT_INT, int(bits[5])),
+            tc=cast(ONE_BIT_INT, int(bits[6])),
+            rd=cast(ONE_BIT_INT, int(bits[7])),
+            ra=cast(ONE_BIT_INT, int(bits[8])),
+            z=cast(THREE_BIT_INT, int(bits[9:12], 2)),
+            rcode=cast(FOUR_BIT_INT, int(bits[12:16], 2)),
+        )
+
+    def encode(self):
+        # Construct the 16-bit flags field based on the individual components
+        # Uses big endian format (bits are placed in reverse order)
+        return (
+            (self.qr << 15)
+            | (self.opcode << 11)  # opcode is 4 bits 11 till 14th position
+            | (self.aa << 10)
+            | (self.tc << 9)
+            | (self.rd << 8)
+            | (self.ra << 7)
+            | (self.z << 4)
+            | self.rcode
+        ).to_bytes(2, "big")
+
+
+@dataclass
+class Header:
+    id: int
+    flags: Flags
+    qdcount: int
+    ancount: int
+    nscount: int
+    arcount: int
+
+    @classmethod
+    def from_bytes(cls, b_msg: bytes) -> "Header":
+        b_io = io.BytesIO(b_msg)
+        id = int.from_bytes(b_io.read(2), "big")
+        flags = Flags.from_bytes(b_io.read(2))
+        qdcount = int.from_bytes(b_io.read(2), "big")
+        ancount = int.from_bytes(b_io.read(2), "big")
+        nscount = int.from_bytes(b_io.read(2), "big")
+        arcount = int.from_bytes(b_io.read(2), "big")
+        return cls(
+            id=id,
+            flags=flags,
+            qdcount=qdcount,
+            ancount=ancount,
+            nscount=nscount,
+            arcount=arcount,
+        )
+
+    def encode(self) -> bytes:
+        return b"".join(
+            [
+                self.id.to_bytes(2, "big"),
+                self.flags.encode(),
+                self.qdcount.to_bytes(2, "big"),
+                self.ancount.to_bytes(2, "big"),
+                self.nscount.to_bytes(2, "big"),
+                self.arcount.to_bytes(2, "big"),
+            ]
+        )
+
+
+@dataclass
+class DnsMessage:
+    header: Header
+
+    @classmethod
+    def from_bytes(cls, b_msg: bytes):
+        b_io = io.BytesIO(b_msg)
+        b_header = b_io.read(12)
+        return cls(header=Header.from_bytes(b_header))
+
+    def encode(self) -> bytes:
+        return self.header.encode()
