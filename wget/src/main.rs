b1353e5ff30a97dd786e35cb191ead6a919a057f
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-04-14 19:15:09 +0545

    feat: add clap for args parsing

--- wget/src/main.rs
+++ wget/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Wget clone written in Rust
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Url to download
+    #[arg(short, long)]
+    url: String,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 }
