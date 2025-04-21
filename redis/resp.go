9c2ce558e9737893004a7a2d2f4ca4a5eeaa8a0f
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-04-21 18:41:50 +0545

    feat: add writer func and implementation

--- redis/resp.go
+++ redis/resp.go
@@ -10,6 +10,7 @@ import (
 const (
 	STRING  = '+'
 	ERROR   = '-'
+	NULL    = '_'
 	INTEGER = ':'
 	BULK    = '$'
 	ARRAY   = '*'
@@ -112,4 +113,92 @@ func (r *Resp) readArray() (Value, error) {
 	}
 
 	return value, nil
+}
+
+func (v Value) Marshal() []byte {
+	switch v.typ {
+	case "array":
+		return v.marshalArray()
+
+	case "bulk":
+		return v.marshalBulk()
+
+	case "string":
+		return v.marshalString()
+
+	case "null":
+		return v.marshalNull()
+
+	case "error":
+		return v.marshalError()
+	default:
+		return []byte{}
+
+	}
+}
+
+func (v Value) marshalString() []byte {
+	var bytes []byte
+	bytes = append(bytes, STRING)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+	return bytes
+}
+
+func (v Value) marshalNull() []byte {
+	return []byte("$-1\r\n")
+	// var bytes []byte
+	// bytes = append(bytes, NULL)
+	// bytes = append(bytes, '\r', '\n')
+	//
+	// return bytes
+}
+
+func (v Value) marshalError() []byte {
+	var bytes []byte
+	bytes = append(bytes, ERROR)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+
+	return bytes
+}
+
+func (v Value) marshalBulk() []byte {
+	var bytes []byte
+	bytes = append(bytes, BULK)
+	bytes = append(bytes, strconv.Itoa(len(v.bulk))...)
+	bytes = append(bytes, '\r', '\n')
+	bytes = append(bytes, v.bulk...)
+	bytes = append(bytes, '\r', '\n')
+	return bytes
+}
+
+func (v Value) marshalArray() []byte {
+	var bytes []byte
+	bytes = append(bytes, ARRAY)
+	bytes = append(bytes, strconv.Itoa(len(v.array))...)
+	bytes = append(bytes, '\r', '\n')
+	for i := range len(v.array) {
+		bytes = append(bytes, v.array[i].Marshal()...)
+	}
+	return bytes
+}
+
+type Writer struct {
+	writer io.Writer
+}
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{writer: w}
+}
+
+func (w *Writer) Write(v Value) error {
+	bytes := v.Marshal()
+
+	_, err := w.writer.Write(bytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
