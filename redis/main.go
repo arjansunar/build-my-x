3bba59269c7068345e0b6ca0e52bf71410293a16
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-04-21 17:47:18 +0545

    feat: add tcp connections

--- redis/main.go
+++ redis/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
 	fmt.Println("Hello World")
 }
